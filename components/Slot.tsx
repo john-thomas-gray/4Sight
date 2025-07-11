dd9a33d7009995f9683268323d53252c1f176bae
John <john.thom.gray@gmail.com>
2025-07-10 19:16:10 -0700

    color

--- components/Slot.tsx
+++ components/Slot.tsx
@@ -46,13 +46,32 @@ const Slot = ({ id, register, orientation, team = "white" }: SlotProps) => {
     height: 40,
     alignItems: "center",
     justifyContent: "center",
+    backgroundColor: "#6E2C00", // Deep reddish brown
+    borderWidth: 2,
+    borderColor: "#C0C0C0", // Silver border
+    position: "relative", // Needed for absolutely positioned circle
   };
 
   return (
     <View ref={viewRef} style={style}>
+      <View
+        style={{
+          position: "absolute",
+          width: 28,
+          height: 28,
+          borderRadius: 14,
+          backgroundColor: "#C0C0C0", // Silver circle
+          zIndex: 0,
+        }}
+      />
       <Image
         source={icons.slot[dir][team]}
-        style={{ width: 40, height: 40, resizeMode: "contain" }}
+        style={{
+          width: 24,
+          height: 24,
+          resizeMode: "contain",
+          zIndex: 1,
+        }}
       />
     </View>
   );
