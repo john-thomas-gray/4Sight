22fc09453123a29aabc38d42c93cacd1296e9f7d
John <john.thom.gray@gmail.com>
2025-08-29 17:45:15 -0700

    workletized

--- components/Piece.tsx
+++ components/Piece.tsx
@@ -1,5 +1,4 @@
 import { Animations, GameElements } from "@/constants";
-import { BOARD_SIZE_ZERO_IDX } from "@/constants/gameElements";
 import { useGameContext } from "@/context/GameContext";
 import useBoardPullAnimation from "@/hooks/useBoardPullAnimation";
 import { usePieceState } from "@/hooks/usePieceState";
@@ -7,6 +6,7 @@ import { Board } from "@/types";
 import { HighlightProps, PieceProps, Team } from "@/types/board";
 import { GameState } from "@/types/logic";
 import { getCellArray } from "@/utils/boardLogic";
+import getReachableSlot from "@/utils/getReachableSlot";
 import React, { useEffect } from "react";
 import { ViewStyle } from "react-native";
 import { Gesture, GestureDetector } from "react-native-gesture-handler";
@@ -296,115 +296,6 @@ const Piece = ({
 
           runOnJS(setBPLUI)(finalSpaceId);
         } else if (isSpace) {
-          const getReachableSlot = (
-            board: Record<string, string>,
-            targetCell: string
-          ) => {
-            const [row, col] = targetCell.split("-").map(Number);
-
-            const distanceToSlot: Record<string, number> = {
-              Up: row,
-              Down: BOARD_SIZE_ZERO_IDX - row,
-              Right: BOARD_SIZE_ZERO_IDX - col,
-              Left: col,
-            };
-
-            const directionVectors: Record<string, [number, number]> = {
-              Up: [-1, 0],
-              Down: [1, 0],
-              Right: [0, 1],
-              Left: [0, -1],
-            };
-
-            const direction: Record<string, string> = {
-              Up: "Up",
-              Down: "Down",
-              Right: "Right",
-              Left: "Left",
-            };
-
-            const reverseDirection: Record<string, string> = {
-              Up: "Down",
-              Down: "Up",
-              Right: "Left",
-              Left: "Right",
-            };
-
-            const cellStatus = (r: number, c: number, step: number) => {
-              if (r === 0 || r === 8 || c === 0 || c === 8) {
-                return step === 1 ? "slotNeighbor" : "slot";
-              }
-              const piece = board[`${r}-${c}`];
-              if (piece) {
-                return step === 1 ? "neighbor" : "blocked";
-              }
-
-              return "empty";
-            };
-
-            const layout: Record<string, any> = {};
-            layout.dropSlot = { id: "null", distance: 99 };
-
-            if (cellStatus(row, col, 0) === "blocked")
-              return (layout.dropSlot = { id: "abort", distance: 99 });
-
-            for (const dir of Object.values(direction)) {
-              const [dRow, dCol] = directionVectors[dir];
-              const distance = distanceToSlot[dir];
-
-              for (let i = 1; i <= distance; i++) {
-                const r = row + dRow * i;
-                const c = col + dCol * i;
-                const id = `${r}-${c}`;
-                if (r > 8 || r < 0 || c > 8 || c < 0) {
-                  return (layout.dropSlot = {
-                    id: "out of bounds",
-                    distance: 99,
-                  });
-                }
-                const status = cellStatus(r, c, i);
-
-                if (status === "empty") {
-                  layout[dir] = { cell: status, id: id, distance: i };
-                  continue;
-                }
-
-                if (status === "slotNeighbor") {
-                  layout[dir] = { cell: status, id: id, distance: i };
-                  break;
-                } else if (status === "neighbor") {
-                  layout[dir] = { cell: status, id: id, distance: i };
-                  break;
-                } else if (status === "blocked") {
-                  layout[dir] = { cell: status, id: id, distance: i };
-                  break;
-                } else if (status === "slot") {
-                  layout[dir] = { cell: status, id: id, distance: i };
-                  break;
-                }
-              }
-            }
-
-            for (const dir of Object.keys(direction)) {
-              const reverse = reverseDirection[dir];
-              if (
-                (layout[dir].cell === "slot" &&
-                  layout[reverse].cell === "neighbor") ||
-                (layout[dir].cell === "slot" &&
-                  layout[reverse].cell === "slotNeighbor") ||
-                (layout[dir].cell === "slotNeighbor" &&
-                  layout[reverse].cell === "neighbor")
-              ) {
-                if (layout[dir].distance < layout.dropSlot.distance) {
-                  layout.dropSlot = {
-                    id: layout[dir].id,
-                    distance: layout[dir].distance,
-                  };
-                }
-              }
-            }
-            return layout;
-          };
           const dropSlotData = getReachableSlot(layout.boardPieceLocations, id);
           if (dropSlotData.dropSlot) {
             const slotData = slots.find(
--- utils/getReachableSlot.ts
+++ utils/getReachableSlot.ts
@@ -4,8 +4,7 @@ const getReachableSlot = (
   board: Record<string, string>,
   targetCell: string
 ) => {
-  ("worklet");
-
+  "worklet";
   const [row, col] = targetCell.split("-").map(Number);
 
   const distanceToSlot: Record<string, number> = {
@@ -48,20 +47,11 @@ const getReachableSlot = (
     return "empty";
   };
 
-  const slotViable = (dir: string, distance: number, id: string) => {
-    const reverse = reverseDirection[dir];
-    if (layout[reverse]?.cell === "neighbor") {
-      if (!dropSlotData || distance < dropSlotData.distance) {
-        return (dropSlotData = { id, distance });
-      }
-    }
-    return null;
-  };
-
   const layout: Record<string, any> = {};
-  let dropSlotData: { id: string; distance: number } | null = null;
+  layout.dropSlot = { id: "null", distance: 99 };
 
-  if (cellStatus(row, col, 0) === "blocked") return null;
+  if (cellStatus(row, col, 0) === "blocked")
+    return (layout.dropSlot = { id: "abort", distance: 99 });
 
   for (const dir of Object.values(direction)) {
     const [dRow, dCol] = directionVectors[dir];
@@ -72,33 +62,52 @@ const getReachableSlot = (
       const c = col + dCol * i;
       const id = `${r}-${c}`;
       if (r > 8 || r < 0 || c > 8 || c < 0) {
-        return null;
+        return (layout.dropSlot = {
+          id: "out of bounds",
+          distance: 99,
+        });
       }
       const status = cellStatus(r, c, i);
+
+      if (status === "empty") {
+        layout[dir] = { cell: status, id: id, distance: i };
+        continue;
+      }
+
       if (status === "slotNeighbor") {
-        if (slotViable(dir, distance, id)) {
-          dropSlotData = { id, distance };
-          // return dropSlotData;
-        } else {
-          layout[dir] = { cell: "neighbor" };
-        }
+        layout[dir] = { cell: status, id: id, distance: i };
+        break;
       } else if (status === "neighbor") {
-        layout[dir] = { cell: status };
+        layout[dir] = { cell: status, id: id, distance: i };
         break;
       } else if (status === "blocked") {
-        layout[dir] = { cell: status };
+        layout[dir] = { cell: status, id: id, distance: i };
         break;
       } else if (status === "slot") {
-        layout[dir] = { cell: status, distance: i };
-        if (slotViable(dir, distance, id)) {
-          dropSlotData = slotViable(dir, distance, id) || null;
-        }
+        layout[dir] = { cell: status, id: id, distance: i };
         break;
       }
     }
   }
 
-  return dropSlotData;
+  for (const dir of Object.keys(direction)) {
+    const reverse = reverseDirection[dir];
+    if (
+      (layout[dir].cell === "slot" && layout[reverse].cell === "neighbor") ||
+      (layout[dir].cell === "slot" &&
+        layout[reverse].cell === "slotNeighbor") ||
+      (layout[dir].cell === "slotNeighbor" &&
+        layout[reverse].cell === "neighbor")
+    ) {
+      if (layout[dir].distance < layout.dropSlot.distance) {
+        layout.dropSlot = {
+          id: layout[dir].id,
+          distance: layout[dir].distance,
+        };
+      }
+    }
+  }
+  return layout;
 };
 
 export default getReachableSlot;
